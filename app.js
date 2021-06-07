362302a0d5dcd925462438b371223e8317d372fa
dominik <aplikas7@gmail.com>
2021-06-07 20:20:38 +0200

    socket rooms

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const port = process.env.PORT ?? 80
 
+const e = require('express');
 var express = require('express')
 var app = express()
 var http = require('http').Server(app)
@@ -13,8 +14,13 @@ app.get('/', function (req, res) {
 })
 
 
-var players = {}
-var deckLastCount
+class Room {
+  constructor() {
+    this.players = {}
+    this.deckLastCount = undefined
+  }
+}
+
 
 class Player {
   constructor(id) {
@@ -27,84 +33,133 @@ class Player {
 }
 
 
-function playersReady() {
-  for({ready} of Object.values(players)) {
+rooms = {
+  'main': new Room()
+}
+
+function getRoom(socket) {
+  foo = new Set(socket.rooms)
+  foo.delete(socket.id)
+  return foo.values().next().value
+}
+
+
+function playersReady(roomId) {
+  for({ready} of Object.values(rooms[roomId].players)) {
     if(!ready) return false
   }
   return true
 }
 
 
-function playersChanged() {
-  io.emit('player-list', players)
+function playersChanged(roomId) {
+  io.to(roomId).emit('player-list', rooms[roomId].players)
   
-  if(Object.keys(players).length >= 2 && playersReady()) {
-    roundStart()
+  if(Object.keys(rooms[roomId].players).length >= 2 && playersReady(roomId)) {
+    roundStart(roomId)
   }
 }
 
 
-function roundStart() {
-  for(key of Object.keys(players)) {
-    players[key].ready = false
-    players[key].inGame = true
+function roundStart(roomId) {
+  for(key of Object.keys(rooms[roomId].players)) {
+    rooms[roomId].players[key].ready = false
+    rooms[roomId].players[key].inGame = true
   }
-  deckLastCount = undefined
+  rooms[roomId].deckLastCount = undefined
 
-  io.emit('player-list', players)
-  io.emit('round-start', Date.now())
+  io.to(roomId).emit('player-list', rooms[roomId].players)
+  io.to(roomId).emit('round-start', Date.now())
 }
 
 
-function roundEnd() {
+function roundEnd(roomId) {
   let scoreList = []
-  for(key of Object.keys(players)) {
-    if(players[key].score != 0) {
-      const { name, score } = players[key]
+  for(key of Object.keys(rooms[roomId].players)) {
+    if(rooms[roomId].players[key].inGame) {
+      const { name, score } = rooms[roomId].players[key]
       scoreList.push([name, score])
     }
   }
   scoreList.sort((a, b) => b[1] - a[1])
   
-  for(key of Object.keys(players)) {
-    players[key].score = 0
-    players[key].inGame = false
+  for(key of Object.keys(rooms[roomId].players)) {
+    rooms[roomId].players[key].score = 0
+    rooms[roomId].players[key].inGame = false
   }
 
-  io.emit('player-list', players)
-  io.emit('round-end', scoreList)
+  io.to(roomId).emit('player-list', rooms[roomId].players)
+  io.to(roomId).emit('round-end', scoreList)
+}
+
+
+function onSymbolSelected(roomId, playerId, symbolId, deckCount) {
+  let deckLastCount = rooms[roomId].deckLastCount
+
+  if(deckCount >= deckLastCount && deckLastCount != undefined) return
+    deckLastCount = deckCount
+    
+    rooms[roomId].players[playerId].score++
+    io.to(roomId).emit('symbol-correct', rooms[roomId].players[playerId], symbolId)
+
+    if(deckCount == 30) {
+      roundEnd(roomId)
+    }
+}
+
+
+function onSetPlayer(roomId, playerId, options) {
+  for([key, value] of Object.entries(options)) {
+    rooms[roomId].players[playerId][key] = value
+  }
+  playersChanged(roomId)
+}
+
+
+function onConnection(socket) {
+  let roomId = 'main'
+  let playerId = socket.id
+  socket.join(roomId)
+
+  if(!rooms.hasOwnProperty(roomId)) {
+    rooms[roomId] = new Room()
+  }
+
+  rooms[roomId].players[playerId] = new Player(playerId)
+  playersChanged(roomId)
+}
+
+
+function onDisconnection(roomId, playerId) {
+  delete rooms[roomId].players[playerId]
+
+  if(Object.keys(rooms[roomId].players).length === 0) {
+    delete rooms[roomId]
+  }
+  else {
+    playersChanged(roomId)
+  }
 }
 
 
 io.on('connection', (socket) => {
-  players[socket.id] = new Player(socket.id)
-  playersChanged()
+  onConnection(socket)
 
+  // socket.on('set-room', (roomId) => {
+  //   socket.join(roomId)
+  //   socket.emit('player-list', rooms[roomId].players)
+  // })
 
   socket.on('set-player', (options) => {
-    for([key, value] of Object.entries(options)) {
-      players[socket.id][key] = value
-    }
-    playersChanged()
+    onSetPlayer(getRoom(socket), socket.id, options)
   })
-
 
   socket.on('symbol-selected', (symbolId, deckCount) => {
-    if(deckCount >= deckLastCount && deckLastCount != undefined) return
-    deckLastCount = deckCount
-    
-    players[socket.id].score++
-    io.emit('symbol-correct', players[socket.id], symbolId)
-
-    if(deckCount == 28) {
-      roundEnd()
-    }
+    onSymbolSelected(getRoom(socket), socket.id, symbolId, deckCount)
   })
 
-
-  socket.on('disconnect', (reason) => {
-    delete players[socket.id]
-    playersChanged()
+  socket.on("disconnecting", () => {
+    onDisconnection(getRoom(socket), socket.id)
   })
 })
 
